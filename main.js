f599c24a68429055c1f856634b994719ab947775
agheadley <agheadley@gmail.com>
2017-08-04 15:20:19 +0100

    Initial commit

--- main.js
+++ main.js
@@ -1,57 +1,92 @@
-// Matter.js module aliases
-var Engine = Matter.Engine,
-    //Render = Matter.Render,
-    World = Matter.World,
-    Bodies = Matter.Bodies;
-    Body=Matter.Body;
+var canvas,ctx;
 
-var engine,world;
-var ground1;
+var world;
+var ball=[];
 
-var walls=[];
+var viewWidth=400;
+var viewHeight=400;
 
-var atoms=[];
+function main() {
+
+    canvas=document.getElementById('canvas');
+    ctx=canvas.getContext('2d');
+
+    world=Physics({timestep:1000.0/160,maxIPF:16,integrator:'verlet'});
+    var gravity=Physics.behavior('constant-acceleration',{x:0,y:0.004});
+    gravity.setAcceleration({x:0,y:0}); //default
+    world.add(gravity);
+
+    var viewportBounds = Physics.aabb(0, 0, viewWidth, viewHeight);
+
+    // add borders 
+    var viewPort=Physics.behavior('edge-collision-detection', {
+      aabb: viewportBounds,
+      restitution: 1.0,
+      cof: 1.0
+    });
+
+    world.add(viewPort);    
+    world.add(Physics.behavior('body-impulse-response'));
+
+    // add body collision detection and resolution
+    world.add(Physics.behavior('body-collision-detection'));
+    world.add(Physics.behavior('sweep-prune'));
 
 
-function setup()
+
+    for(var i=0;i<20;i++) 
+        ball[i]=new Ball(300*Math.random(),300*Math.random(),10,-0.1+0.2*Math.random(),-0.1+0.2*Math.random());
+    
+    //console.log(ball.physics.state.pos.values());
+    //console.log(ball.physics.state.vel.values());
+    
+    // subscribe to ticker to advance the simulation
+    Physics.util.ticker.on(function( time, dt ){
+      world.step( time );
+    });
+
+    // start the ticker
+    Physics.util.ticker.start();
+
+    // render on each step
+    //world.on('step', function(){    
+    //    ball.show();
+    //});
+
+    world.on('step',step);
+
+}
+
+function step() {
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    for(var i=0;i<ball.length;i++) ball[i].show();
+    //ball.show();
+}
+
+function Ball(posx,posy,rad,velx,vely)
 {
-  createCanvas(400, 400);
-  rectMode(CENTER);
-  // create an engine
-  engine = Engine.create();
-  world=engine.world;
-  world.gravity.x=0;
-  world.gravity.y=0;
+    
+    var options={x:posx,y:posy,vx:velx,vy:vely,radius:rad,cof:0,restitution:1.0};
+    
+    this.physics = Physics.body('circle',options);
+    
+    world.add(this.physics);
 
-  Engine.run(engine);
+    this.show=function()
+    {
+        var pos=this.physics.state.pos.values();
+        var angle=this.physics.state.angular.pos;
+        var rad=this.physics.radius;
+        //ctx.fillRect(pos.x,pos.y,3,3);
+        ctx.beginPath();
+        ctx.arc(pos.x,pos.y,rad,0,2*Math.PI);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(pos.x,pos.y);
+        ctx.lineTo(pos.x+rad*Math.cos(angle),pos.y+rad*Math.sin(angle));
+        ctx.stroke();
 
-  var options = {isStatic:true,restitution:1.01,friction:0,frictionAir:0,frictionStatic:0};
-  walls[0]= new Rectangle(width/2,height,width,1,options);
-  walls[1]= new Rectangle(width/2,0,width,1,options);
-  walls[2]= new Rectangle(width,height/2,1,height,options);
-  walls[3]= new Rectangle(0,height/2,1,height,options);
-
-
-
+        //console.log(pos);
+    }
 }
 
-function mouseDragged()
-{
-  var options = {restitution:1,friction:0,frictionAir:0,frictionStatic:0,force:{x:(-0.01+Math.random()/50),y:(-0.01+Math.random()/50)},mass:1};
-  atoms.push(new Circle(mouseX,mouseY,10,options));
-  console.log(atoms[atoms.length-1].body.velocity);
-
-  return false;
-}
-
-function draw()
-{
-background('#FFFFFF');
-//rect(box1.position.x,box1.position.y,80,80);
-for(var i=0;i<walls.length;i++) walls[i].show();
-for(var i=0;i<atoms.length;i++) {
-  atoms[i].show();
-  //atoms[i].body.force.x=0.1;
-}
-
-}
